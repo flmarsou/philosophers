8a6d3e5851874dd38e6598ee81f05a3448627744
flmarsou <marsouinflorent12@gmail.com>
2024-09-26 16:07:02 +0200

    Slight Update

--- src/routines/routine.c
+++ src/routines/routine.c
@@ -6,24 +6,11 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:03:30 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/09/25 15:59:05 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/09/26 11:17:58 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
-
-void	routine_eat(t_philos *philo)
-{
-	if (philo->ate)
-		return ;
-	pthread_mutex_lock(&philo->left_fork);
-	pthread_mutex_lock(philo->right_fork);
-	printf("Current time: %lu | Philo ID: %u ate.\n", ft_gettime() - philo->stats->timestamp, philo->id);
-	ft_msleep(philo->stats->time_to_eat);
-	philo->ate = 1;
-	pthread_mutex_unlock(&philo->left_fork);
-	pthread_mutex_unlock(philo->right_fork);
-}
 
 void	*routine(void *arg)
 {
@@ -39,6 +26,7 @@ void	*routine(void *arg)
 	while (1)
 	{
 		routine_eat(philo);
+		routine_sleep(philo);
 	}
 	return (0);
 }
