db42ec3046b4d3b50823c5b90bc4348caa0a9053
flmarsou <marsouinflorent12@gmail.com>
2024-11-13 15:28:41 +0100

    Major Update

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 12:32:28 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/09/27 13:00:12 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/11/13 14:48:22 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,17 @@
 # include <pthread.h>	// pthread_create, pthread_detach, pthread_join,
 						// pthread_mutex_init, pthread_mutex_destroy
 						// pthread_mutex_lock, pthread_mutex_unlock
+# include <stdbool.h>	// Booleans
 
-// Boolean
-typedef enum e_bool
+// Action Enums
+typedef enum e_actions
 {
-	false = 0,
-	true = 1
-}	t_bool;
+	FORK,
+	EAT,
+	SLEEP,
+	THINK,
+	DEAD
+}	t_actions;
 
 // Philosophers' Stats
 struct s_stats
@@ -38,9 +42,6 @@ struct s_stats
 	unsigned int	time_to_sleep;	// Time to sleep in miliseconds
 	unsigned int	cycles;			// Number of cycles
 	unsigned long	timestamp;		// Current time in milisecond
-	t_bool			key;			// Bool to wait for all threads to init
-	t_bool			is_dead;		// Bool to check if a philosopher died
-	pthread_mutex_t	died;
 };
 
 // Philosophers' Info (Individual Allocation)
@@ -50,36 +51,46 @@ typedef struct s_philos
 	pthread_mutex_t	left_fork;		// Philosophers' Fork
 	pthread_mutex_t	*right_fork;	// Philosophers' Neighbor Fork
 	pthread_t		thread;			// Philosophers' Thread
-	t_bool			ate;			// Bool to check if a philosopher ate
-	unsigned long	time_left;
+	unsigned long	time_left;		// Time left before dying
+	bool			is_dead;		// Bool to check if a philosopher died
+	pthread_mutex_t	is_dead_mutex;
 	struct s_stats	*stats;
 }	t_philos;
 
 // Main Struct
 typedef struct s_sim
 {
+	pthread_t		monitor;		// Monitor thread that checks for death
 	struct s_stats	stats;
-	struct s_philos	*philos;			// Array of Philosophers
+	struct s_philos	philos[200];	// Array of Philosophers
 }	t_sim;
 
 //===============================//
 //             Utils             //
 //===============================//
+
 unsigned int	ft_atou(const char *str);
 unsigned long	ft_gettime(void);
 
 //===============================//
 //             Parser            //
 //===============================//
-t_bool			parser(int argc, const char **argv);
+
+bool			parser(int argc, const char **argv);
 
 //===============================//
 //            Threads            //
 //===============================//
+
+void			*routine_monitor(void *arg);
 void			*routine(void *arg);
-// void			routine_eat(t_philos *philo);
-// void			routine_sleep(t_philos *philo);
-void			rest(unsigned int time_to_wait, t_philos *philo);
+
+void			routine_eat(t_philos *philo);
+void			routine_sleep(t_philos *philo);
+void			routine_think(t_philos *philo);
+
+void			rest(unsigned int time_to_wait);
+
 void			printer(unsigned long time, unsigned int id, unsigned int act);
 
 #endif
