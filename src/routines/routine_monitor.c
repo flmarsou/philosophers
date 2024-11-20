a2b4bdc5db90d118e547678c9443a61b3b653041
flmarsou <marsouinflorent12@gmail.com>
2024-11-20 13:46:05 +0100

    Added death check to monitor thread

--- src/routines/routine_monitor.c
+++ src/routines/routine_monitor.c
@@ -6,7 +6,7 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 10:43:31 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/11/13 15:10:47 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/11/20 13:43:38 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,28 @@
 
 void	*routine_monitor(void *arg)
 {
-	t_philos		*philo;
+	t_philos		*philos;
+	unsigned int	i;
 
-	philo = (t_philos *)arg;
+	philos = (t_philos *)arg;
+	i = 0;
 	while (true)
 	{
-		sleep(1);
+		i = 0;
+		while (i < philos->stats->nbr_of_philos)
+		{
+			pthread_mutex_lock(&philos->stats->lock);
+			if (ft_gettime() - philos[i].time_left > philos->stats->time_to_die)
+			{
+				philos->stats->stop = true;
+				printer((ft_gettime() - philos->stats->timestamp), philos->id, DEAD);
+				pthread_mutex_unlock(&philos->stats->lock);
+				return (NULL);
+			}
+			i++;
+			pthread_mutex_unlock(&philos->stats->lock);
+		}
+		usleep(500);
 	}
 	return (NULL);
 }
