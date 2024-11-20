d80e3a585b5e591f7a560a7ed9205e84e4e60e37
flmarsou <marsouinflorent12@gmail.com>
2024-11-20 13:45:24 +0100

    Small Update

--- src/routines/printer.c
+++ src/routines/printer.c
@@ -6,19 +6,20 @@
 /*   By: flmarsou <flmarsou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 10:47:28 by flmarsou          #+#    #+#             */
-/*   Updated: 2024/11/13 10:37:53 by flmarsou         ###   ########.fr       */
+/*   Updated: 2024/11/20 13:22:40 by flmarsou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/philo.h"
 
+// Fancy Printer
 void	printer(unsigned long time, unsigned int id, unsigned int act)
 {
 	const char	fork[25] = "\e[1mhas taken a fork\e[0m";
 	const char	eat[25] = "\e[1mis eating\e[0m       ";
 	const char	sleep[25] = "\e[1mis sleeping     \e[0m";
 	const char	think[25] = "\e[1mis thinking     \e[0m";
-	const char	death[25] = "\e[1mdied            \e[0m";
+	const char	death[27] = "\e[31;1mdied            \e[0m";
 
 	if (act == FORK)
 		printf("║ \e[1m%8lu\e[0m ║ \e[1m%8u\e[0m ║ %s ║\n", time, id, fork);
@@ -35,13 +36,14 @@ void	printer(unsigned long time, unsigned int id, unsigned int act)
 	}
 }
 
+// Normal Printer
 // void	printer(unsigned long time, unsigned int id, unsigned int act)
 // {
 // 	const char	fork[25] = "\e[1mhas taken a fork\e[0m";
 // 	const char	eat[25] = "\e[1mis eating\e[0m";
 // 	const char	sleep[25] = "\e[1mis sleeping\e[0m";
 // 	const char	think[25] = "\e[1mis thinking\e[0m";
-// 	const char	death[25] = "\e[1mdied\e[0m";
+// 	const char	death[25] = "\e[31;1mdied\e[0m";
 
 // 	if (act == FORK)
 // 		printf("%lu %u %s\n", time, id, fork);
